ffd1b86c987c396b2ecc63e9bfb512bcf444326f
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-07-04 05:11:59 +0800

    project section set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 // import Footer from "./components/Footer";
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
       <Navbar />
       <Hero />  
       <About />
-      {/*  <Projects />
-      <Footer /> */}
+      <Projects />
+       {/* <Footer /> */}
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,80 @@
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <>
+    <img src="wave-haikei.svg" alt="Blurry gradient background" className="projects-bg" />
+    <section className="projects">
+      
+      <h2>My Projects</h2>
+      <div className="cards-container">
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="public/kf-pastries.png" alt="Kuya Fidel's Pastries" />
+          <div className="card-text-content">
+            <h3>Kuya Fidel's Pastries</h3>
+            <p>Lorem10 ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default Projects;
