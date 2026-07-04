c6e448b8b285ac2966a7c2664f5989ab9beec9e1
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-07-04 15:24:00 +0800

    add wrapper to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,21 @@ function Hero() {
   return (
     <section className="hero">
         <img src="blurry-gradient-haikei (4).svg" alt="Blurry gradient background" class="hero-bg" />
-
-        <div className="hero-text">
-            <p>Hello!</p>
-            <h3>I'm Robin</h3>       
-            <h1>Web Developer with Business Mindset</h1> 
-            <h4>I build responsive websites, POS systems, and practical business solutions using React, Laravel, JavaScript, and MySQL.</h4>
-            
-            <div className="hero-btns">
-                <button className="vp-btn">View Projects</button>
-                <button className="dr-btn">Download Resume</button>  
+        <div className="hero-wrapper">
+            <div className="hero-text">
+                <p>Hello!</p>
+                <h3>I'm Robin</h3>       
+                <h1>Web Developer with Business Mindset</h1> 
+                <h4>I build responsive websites, POS systems, and practical business solutions using React, Laravel, JavaScript, and MySQL.</h4>
+                
+                <div className="hero-btns">
+                    <button className="vp-btn">View Projects</button>
+                    <button className="dr-btn">Download Resume</button>  
+                </div>
             </div>
-        </div>
-        <div className="hero-image">
-            <img src="public/hero-img.png   " alt="" />
+            <div className="hero-image">
+                <img src="public/hero-img.png   " alt="" />
+            </div>
         </div>
     </section>
   );
