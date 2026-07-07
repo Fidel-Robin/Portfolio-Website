c997a7d90a154e1bee929eec591d2302aa26a57f
Ishmael Robin Fidel <fidelishmaelrobin12@gmail.com>
2026-07-07 11:53:20 +0800

    project details set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/Projects";
+import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 
